78c0d230e7cf5151084f07dcc4ef063f388cab7a
Abdelrahman Fouad <abdelrahmanfouad.cs@gmail.com>
2024-05-27 18:53:10 +0300

    Flavors with firebase (Not Completed)

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flavors_todo_app/firebase/dev/firebase_options.dart';
 import 'package:flavors_todo_app/flavors.dart';
 import 'package:flavors_todo_app/global.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Global.baseUrl = 'https://flvors-aws/dev';
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptionsDev.currentPlatform,
+  );
   runApp(const MyApp());
 }
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,8 +1,17 @@
 import 'package:flavors_todo_app/flavors.dart';
 import 'package:flavors_todo_app/global.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase/prod/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Global.baseUrl = 'https://flvors-aws/prod';
+
+
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptionsProd.currentPlatform,
+  );
   runApp(const MyApp());
 }
